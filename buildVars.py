3bce0cf3d63c44d5546120cdb1b874b7b1bb0175
mltony <anton.malykh+0@gmail.com>
2024-05-04 11:21:24 -0700

    Update name in buildVars

--- buildVars.py
+++ buildVars.py
@@ -16,22 +16,22 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "taskSwitch",
+	"addon_name": "taskSwitcher",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Task Switch"),
+	"addon_summary": _("Task Switcher"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Task Switch add-on for NVDA provides configurable keystrokes to switch between applications."""),
+	"addon_description": _("""Task Switcher add-on for NVDA provides configurable keystrokes to switch between applications."""),
 	# version
 	"addon_version": "0.1dev",
 	# Author(s)
 	"addon_author": "Tony Malykh <anton.malykh@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/mltony/nvda-task_switch",
+	"addon_url": "https://github.com/mltony/nvda-task-switcher",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/mltony/nvda-google-docs",
+	"addon_sourceURL": "https://github.com/mltony/nvda-task-switcher",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
