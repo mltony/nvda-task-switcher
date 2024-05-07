78c16bb9087d0f539aa362d763b782439b6bf332
mltony <anton.malykh+0@gmail.com>
2024-05-07 16:43:46 -0700

    wip

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 rm addon\globalPlugins\hwndObserver.dll
 rm -r addon\globalPlugins\Win32
 rm -r addon\globalPlugins\x64
@@ -9,4 +10,5 @@ cp cbt_hook\Release\Win32\cbt_hook.dll addon\globalPlugins\Win32\
 cp cbt_hook\Release\Win32\cbt_client.exe addon\globalPlugins\Win32\
 cp cbt_hook\Release\x64\cbt_hook.dll addon\globalPlugins\x64\
 cp cbt_hook\Release\x64\cbt_client.exe addon\globalPlugins\x64\
-scons -c && scons
+::scons -c && scons
+echo  
